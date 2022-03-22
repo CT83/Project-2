d10f5d559e13338f4863b771566f3787363c6d30
Mithil Poojary <mithil467@gmail.com>
2022-03-22 15:05:52 +0530

    Cleanup and restore feature

--- app/app.py
+++ app/app.py
@@ -1,16 +1,19 @@
-from concurrent.futures.process import _ResultItem
 import os
 
-from flask import Flask, jsonify, redirect, render_template, request, url_for, send_file
+from flask import Flask, jsonify, redirect, render_template, request, url_for, send_file, flash
 from flask_bootstrap import Bootstrap
 from flask_restful import Api, Resource
 from flask_wtf import FlaskForm
 from sqlalchemy import create_engine
-from sqlalchemy.engine import reflection, result
+from sqlalchemy.engine import reflection
 from wtforms import StringField
+from pathlib import Path
+import tempfile
+from sqlalchemy import MetaData
+import contextlib
+
 
 from extensions import db
-from utils import get_connector_for_database
 
 app = Flask(__name__, template_folder="templates")
 api = Api(app)
@@ -19,64 +22,12 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
 app.config["SECRET_KEY"] = "secret"
 app.config["db_url"] = os.environ.get("DATABASE_URL")
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
+app.config['UPLOAD_FOLDER'] = tempfile.mkdtemp()
+app.config["db_type"] = "postgres"
 db.init_app(app)
-
 bootstrap = Bootstrap(app)
-
-
-class Database(db.Model):
-    __table_args__ = {"extend_existing": True}
-    id = db.Column(db.Integer, primary_key=True)
-    host = db.Column(db.String(200), unique=False, nullable=True)
-    port = db.Column(db.Integer)
-    database = db.Column(db.String(200), unique=False, nullable=True)
-    user = db.Column(db.String(200), unique=False, nullable=True)
-    password = db.Column(db.String(200), unique=False, nullable=True)
-    db_type = db.Column(db.String(200), unique=False, nullable=True)
-
-
-class ListTablesDatabaseRes(Resource):
-    def get(self):
-        return {"hello": "world"}
-
-
-class SelectAllTableDatabaseRes(Resource):
-    def post(self):
-        data = request.get_json(force=True)
-        db_id = data["db_id"]
-        table_name = data["table_name"]
-        connector = get_connector_for_database(
-            db_id=db_id,
-        )
-        rows = connector.select_all(table_name=table_name)
-        return jsonify(rows)
-
-
-class ServerRes(Resource):
-    def get(self):
-        return {"hello": "world"}
-
-    def post(self):
-        data = request.get_json(force=True)
-        host = data["host"]
-        port = data["port"]
-        database = data["database"]
-        user = data["user"]
-        password = data["password"]
-        db_type = data["db_type"]
-
-        db_instance = Database(
-            host=host,
-            port=port,
-            database=database,
-            user=user,
-            password=password,
-            db_type=db_type,
-        )
-        db.session.add(db_instance)
-        db.session.commit()
-        return jsonify({"id": db_instance.id})
-
 
 @app.route("/")
 def home():
@@ -94,6 +45,7 @@ def connect():
         db_url = form.db_url.data
         app.config["db_url"] = db_url
         engine = create_engine(db_url, echo=False)
+        app.config["db_type"] = engine.url.database
         return redirect(url_for("home"))
 
     # db_url = request.args.get("url")
@@ -107,8 +59,9 @@ def connect():
 def list_tables():
     engine = create_engine(app.config["db_url"], echo=False)
     result = engine.table_names()
-    result = "Table count - " +  str(len(result)) + '\n' + str(result)
-    return render_template("home.html", result = result)
+    result = "Table count - " + str(len(result)) + "\n" + str(result)
+    return render_template("home.html", result=result)
+
 
 class ExecuteForm(FlaskForm):
     db_url = StringField("")
@@ -124,18 +77,18 @@ def execute():
         rows = ""
 
         for row in result:
-            rows += str(row) + '\n'
+            rows += str(row) + "\n"
 
-        return redirect(url_for("output", result = rows, query = query))
+        return redirect(url_for("output", result=rows, query=query))
     except:
         return redirect(url_for("home"))
 
+
 @app.route("/output", methods=["GET", "POST"])
 def output():
-    result = request.args['result']
-    query = request.args['query']
-    return render_template("home.html", result = result, query = query)
-
+    result = request.args["result"]
+    query = request.args["query"]
+    return render_template("home.html", result=result, query=query)
 
 
 @app.route("/list_indices", methods=["GET"])
@@ -149,35 +102,80 @@ def indices():
             result += str(index)
     if result == "":
         result = "No indices"
-    return render_template("home.html", result = result)
+    return render_template("home.html", result=result)
+
 
 from sqlalchemy_schemadisplay import create_schema_graph
 from sqlalchemy import MetaData
 
+
 @app.route("/er", methods=["GET"])
 def er():
     engine = create_engine(app.config["db_url"], echo=False)
-    graph = create_schema_graph(metadata=MetaData(app.config["db_url"]),
+    graph = create_schema_graph(
+        metadata=MetaData(app.config["db_url"]),
         show_datatypes=True,
         show_indexes=True,
-        rankdir='LR',
-        concentrate=False
+        rankdir="LR",
+        concentrate=False,
     )
-    graph.write_png('static/er.png') # write out the file
+    graph.write_png("static/er.png")  # write out the file
     return render_template("er.html")
+
 
 import subprocess
 
+
 @app.route("/backup", methods=["GET"])
 def backup():
+    print("Type of database - ", app.config["db_type"])
     f = open("backup.db", "w")
-    # If db is postgres
-    subprocess.call(["pg_dump", app.config["db_url"]]   , stdout=f)
-    return  send_file(f.name)
+    if app.config["db_type"] == "postgres":
+        subprocess.call(["pg_dump", app.config["db_url"]], stdout=f)
+    if app.config["db_type"] == "mysql":
+        # mysqldump db_name > backup-file.sql
+        subprocess.call(["mysqldump", app.config["db_url"]], stdout=f)
+    return send_file(f.name)
 
+@app.route("/restore", methods=["GET", "POST"])
+def restore():
 
-api.add_resource(ServerRes, "/servers")
-api.add_resource(SelectAllTableDatabaseRes, "/select-all")
+    if request.method == "GET":
+        return render_template("restore.html")
+    file = request.files["myfile"]
+
+    if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+    if file:
+        filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        restore_from_file(file)
+
+        return redirect(url_for("home"))
+    return redirect(url_for("home"))
+
+def restore_from_file(file):
+    file_path = Path(app.config['UPLOAD_FOLDER']) / file.filename
+    print("file path ",file_path)
+    
+    # mysql db_name < backup-file.sql
+    # psql -U username -d dbname < filename.sql
+
+    f = open(file_path)
+    engine = create_engine(app.config["db_url"], echo=False)
+
+    if app.config["db_type"] == "postgres":
+        engine.execute("DROP SCHEMA public CASCADE; CREATE SCHEMA public;")
+        subprocess.call(["psql", app.config["db_url"]], stdin=f)
+    if app.config["db_type"] == "mysql":
+        # mysqldump db_name > backup-file.sql
+        subprocess.call(["mysql", app.config["db_url"]], stdin=f)
+    
+    flash("Restored database succesfully")
+    return redirect(url_for("home"))
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
--- app/db_connectors/sqlite_connector.py
+++
@@ -1,9 +0,0 @@
-class SQLAlchemyConnector:
-    def __init__(self):
-        pass
-
-    def select_all(self):
-        pass
-
-    def create_table(self):
-        pass
